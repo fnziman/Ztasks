bd0e10d1257ccdda5867b5ffe7c0aa6e67d9893c
Frankie Ziman <FNZiman@gmail.com>
2017-08-23 14:25:48 -0400

    COMPLETED AUTH

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
 User.create!(
   username: "demo_login",
   first_name: "Demo",
