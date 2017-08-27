c51b2945a64f842944d5155a9f47bc7e97ab6bf6
Frankie Ziman <FNZiman@gmail.com>
2017-08-27 18:01:28 -0400

    added list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -13,4 +13,5 @@ class List < ActiveRecord::Base
   validates :title, :user_id, presence: true
 
   belongs_to :user
+  has_many :tasks
 end
---
+++ app/models/task.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id            :integer          not null, primary key
+#  title         :string           not null
+#  notes         :text
+#  due_date      :date             not null
+#  completed     :boolean          default(FALSE), not null
+#  list_id       :integer          not null
+#  time_estimate :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class Task < ActiveRecord::Base
+  validates :title, :due_date, :completed, :list_id, presence: true
+
+  belongs_to :list
+end
