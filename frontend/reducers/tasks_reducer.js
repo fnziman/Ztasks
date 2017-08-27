a027a8f19785b406942fcd1d29f7d1e3497dd228
Frankie Ziman <FNZiman@gmail.com>
2017-08-27 19:28:15 -0400

    created tasks reducer

---
+++ frontend/reducers/tasks_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_TASKS,
+         RECEIVE_SINGLE_TASK,
+         REMOVE_TASK,
+         UPDATE_TASK,
+         RECEIVE_CURRENT_TASK
+       } from '../actions/task_actions';
+import _ from 'lodash';
+
+const tasksReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_TASKS:
+      return action.tasks;
+    case RECEIVE_SINGLE_TASK:
+      return _.merge({}, state, { [action.task.id]: action.task });
+    case UPDATE_TASK:
+      return _.merge({}, state, { [action.task.id]: action.task });
+    case REMOVE_TASK:
+      return _.merge({}, state, { [action.task.id]: action.task });
+    default: 
+    return state;
+  }
+};
+
+export default tasksReducer;
