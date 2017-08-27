bfa11a0e3648a898f1a1978bab7c369f0961d41d
Frankie Ziman <FNZiman@gmail.com>
2017-08-27 09:02:55 -0400

    created showLists functionality

--- frontend/components/lists/list_index.jsx
+++ frontend/components/lists/list_index.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import ListIndexItem from './list_index_item';
 
 class ListIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showLists: true };
+    this.toggleLists = this.toggleLists.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchLists();
   }
+  toggleLists() {
+    if (this.state.showLists) {
+      this.setState({ showLists: false });
+    } else {
+      this.setState({ showLists: true });
+    }
+  }
 
   render() {
-
     const lists = this.props.lists.map(list => {
       return (
         <ListIndexItem
@@ -29,12 +40,15 @@ class ListIndex extends React.Component {
     return (
       <container className="list-index-container" >
         <div className="lists-index">
-          <div className="list-header">
+          <div onClick={this.toggleLists} className="list-header">
             <i className="arrow">arrow_drop_down</i>
+
             <h1>Lists</h1>
             <i onClick={this.props.createForm} className="add-list">add</i>
           </div>
-          {lists}
+          <container className={this.state.showLists ? "view" : "hidden"}>
+            {lists}
+          </container>
         </div>
       </container>
     );
