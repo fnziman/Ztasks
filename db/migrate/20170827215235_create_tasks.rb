0e44e412c20f5d940817989da74fb73f2d3444bc
Frankie Ziman <FNZiman@gmail.com>
2017-08-27 17:57:58 -0400

    setup tasks tables

---
+++ db/migrate/20170827215235_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :notes
+      t.date :due_date, null: false
+      t.boolean :completed, null: false, default: false
+      t.integer :list_id, null: false
+      t.integer :time_estimate
+
+      t.timestamps
+    end
+    add_index :tasks, :title
+    add_index :tasks, :list_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823183353) do
+ActiveRecord::Schema.define(version: 20170827215235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 20170823183353) do
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_lists_on_title"
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "notes"
+    t.date "due_date", null: false
+    t.boolean "completed", default: false, null: false
+    t.integer "list_id", null: false
+    t.integer "time_estimate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
   create_table "users", force: :cascade do |t|
