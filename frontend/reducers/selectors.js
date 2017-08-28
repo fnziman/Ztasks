c2d80dbbb98a77d0ab29b778edb9f38f1d4831ce
Frankie Ziman <FNZiman@gmail.com>
2017-08-27 20:41:24 -0400

    craeted tasks selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,5 +1,10 @@
-export const asArray = ({ lists }) => {
+export const ListsAsArray = ({ lists }) => {
   return (
     Object.keys(lists).map(key => lists[key])
+  );
+};
+export const TasksAsArray = ({ tasks }) => {
+  return (
+    Object.keys(tasks).map(key => tasks[key])
   );
 };
