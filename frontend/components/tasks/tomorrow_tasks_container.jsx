b5def511cea2bd49d29842a7eba07c79e36bdd89
Frankie Ziman <FNZiman@gmail.com>
2017-08-29 12:20:45 -0400

    can set and view Tomorrow Tasks

---
+++ frontend/components/tasks/tomorrow_tasks_container.jsx
@@ -0,0 +1,47 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import Tasks from './tasks';
+import { fetchTasks, createTask } from '../../actions/task_actions';
+import { TasksAsArray } from '../../reducers/selectors';
+
+const mapStateToProps = (state, ownProps) => {
+  const allTasks = TasksAsArray(state);
+
+  let currentDate = new Date();
+  let tomorrow = currentDate.setDate(currentDate.getDate() + 1);
+  let year = tomorrow.getFullYear();
+  let month = tomorrow.getMonth();
+  if (month < 10) {
+    month = '0' + (month + 1);
+  }
+  let date = tomorrow.getDate();
+  let formattedDate = year + "-" + month + "-" + date;
+  const listTasks = allTasks.filter(task => {
+    return (task.due_date === formattedDate);
+  });
+  let complete = [];
+  let incomplete = [];
+  listTasks.forEach (task => {
+    if (task.completed) {
+      complete.push(task);
+    } else {
+      incomplete.push(task);
+    }
+  });
+  return {
+    listId: null,
+    dueDate: formattedDate,
+    complete: complete,
+    incomplete: incomplete,
+    currentUser: state.session.currentUser,
+  };
+};
+
+const mapDispatchToProps =  dispatch => {
+  return {
+    fetchTasks: () => dispatch(fetchTasks()),
+    createTask: task => dispatch(createTask(task)),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Tasks));
