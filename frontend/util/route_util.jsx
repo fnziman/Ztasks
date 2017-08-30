8a5d6d850dd08df54e851a8876f908a7aa351c58
Frankie Ziman <FNZiman@gmail.com>
2017-08-30 16:45:04 -0400

    fixed login rerouting issue

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -7,7 +7,7 @@ const Auth = ({component: Component, path, loggedIn}) => (
     !loggedIn ? (
       <Component {...props} />
     ) : (
-      <Redirect to="/app" />
+      <Redirect to="/app/all" />
     )
   )}/>
 );
